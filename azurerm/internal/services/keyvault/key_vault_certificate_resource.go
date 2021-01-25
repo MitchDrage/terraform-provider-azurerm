5659667389415c1ebf6a142943099afd388cba8a
tombuildsstuff <git@ibuildstuff.co.uk>
2021-01-25 13:07:44 +0100

    linting: formatting

--- azurerm/internal/services/keyvault/key_vault_certificate_resource.go
+++ azurerm/internal/services/keyvault/key_vault_certificate_resource.go
@@ -428,7 +428,7 @@ func resourceKeyVaultCertificateCreate(d *schema.ResourceData, meta interface{})
 			}
 		}
 
-		log.Printf("[DEBUG] Waiting for Key Vault Certificate %q in Vault %q to be provisioned", name, keyVaultBaseUrl)
+		log.Printf("[DEBUG] Waiting for Key Vault Certificate %q in Vault %q to be provisioned", name, *keyVaultBaseUrl)
 		stateConf := &resource.StateChangeConf{
 			Pending:    []string{"Provisioning"},
 			Target:     []string{"Ready"},
@@ -445,7 +445,7 @@ func resourceKeyVaultCertificateCreate(d *schema.ResourceData, meta interface{})
 		}
 
 		if _, err := stateConf.WaitForState(); err != nil {
-			return fmt.Errorf("Error waiting for Certificate %q in Vault %q to become available: %s", name, keyVaultBaseUrl, err)
+			return fmt.Errorf("Error waiting for Certificate %q in Vault %q to become available: %s", name, *keyVaultBaseUrl, err)
 		}
 	}
 
